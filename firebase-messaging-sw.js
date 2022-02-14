05d3333013b699a51e38dbd68a2a0ccc0155bc8f
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2022-02-14 22:32:46 +0100

    Decomment all

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -10,9 +10,9 @@ firebase.initializeApp({
 
 firebase.messaging();
 
-// messaging.onBackgroundMessage((payload) => {
-//     const notificationTitle = payload.notification.title;
-//     const notificationOptions = { body: payload.notification.body, icon: '/logo.png' };
+messaging.onBackgroundMessage((payload) => {
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = { body: payload.notification.body, icon: '/logo.png' };
 
-//     self.registration.showNotification(notificationTitle, notificationOptions);
-// }); 
+    self.registration.showNotification(notificationTitle, notificationOptions);
+}); 
