f01c4930916e7cb3a3afaf7cf3c607eec2d88624
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2022-02-23 00:02:54 +0100

    Add CV and email

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -11,6 +11,7 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
+
     const notificationTitle = payload.notification.title;
     const notificationOptions = { body: payload.notification.body, icon: '/logo.png' };
 
