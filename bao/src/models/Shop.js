a6f1c00c82fab1446cf0163cdab3f9041c831103
GiovanniScarpellino <scarpellino.giovanni@gmail.com>
2025-11-02 10:24:19 +0100

    Update BAO

---
+++ bao/src/models/Shop.js
@@ -0,0 +1,15 @@
+export const SHOP_ITEMS = {
+    chick: {
+        name: "Poussin",
+        cost: {
+            food: 10,
+        },
+    },
+    upgrade: {
+        name: "Amélioration",
+        cost: {
+            wood: 10,
+            stone: 10,
+        },
+    },
+};
